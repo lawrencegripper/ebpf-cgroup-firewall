38cec38e8d328d88a28b266814a22d58f3366a6d
lawrencegripper <i+gitgen@grippers.co.uk>
2025-03-13 13:19:52 +0000

    Add some real world tests to run as part of CI
    - Clone and build Traefik
    - Download a Ubuntu ISO

---
+++ script/real-world.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+
+# Start timing the whole process
+start_time=$(date +%s)
+
+cd /tmp
+
+echo "Cloning traefik repository..."
+time git clone https://github.com/traefik/traefik.git
+
+time git checkout v3.3
+
+cd traefik
+
+echo "Running make..."
+time make
+
+echo "Running make lint..."
+time make lint
+
+echo "Running make test..."
+time make test
+
+# Calculate and display the total execution time
+end_time=$(date +%s)
+total_time=$((end_time - start_time))
+echo "Total execution time: $total_time seconds"
