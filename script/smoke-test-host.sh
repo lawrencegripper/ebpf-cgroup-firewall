421d4558d98df931aea05bec2f1db2c2394a3c71
lawrencegripper <i+gitpersonal@grippers.co.uk>
2025-03-17 16:51:34 +0000

    bug: test asserted incorrectly

--- script/smoke-test-host.sh
+++ script/smoke-test-host.sh
@@ -13,7 +13,7 @@ open_fold "IPv6: Block IPv6 Traffic as not supported in the proxy currently (All
     ipv6_address=$(dig hostname A ifconfig.co AAAA +short | tail -n 1)
     # Run it without blocking anything to ensure it's getting stopped before the ip rules are taken into account
     run_firewall_test "--debug --allow-dns-request" "nc -zv -w 1 $ipv6_address 80"
-    assert_exit_code 0
+    assert_exit_code 1
     assert_output_contains "WARN PACKET BLOCKED because=IPv6AlwaysBlocked blocked=true blockedAt=packet"
 close_fold
 
