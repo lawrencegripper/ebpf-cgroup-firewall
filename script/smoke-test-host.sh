7ebcbedfd5ff76a07c7b7b8470b05e9a135d7cb4
lawrencegripper <i+gitgen@grippers.co.uk>
2025-03-17 17:28:53 +0000

    disable test as no ipv6 in actions

--- script/smoke-test-host.sh
+++ script/smoke-test-host.sh
@@ -9,13 +9,16 @@ source "$(dirname "$0")/helpers.sh"
 
 # Why on host only?
 # Docker doesn't configure ipv6 by default, so we can't test it in the container
-open_fold "IPv6: Block IPv6 Traffic as not supported in the proxy currently (Allow DNS)"
-    ipv6_address=$(dig hostname A ifconfig.co AAAA +short | tail -n 1)
-    # Run it without blocking anything to ensure it's getting stopped before the ip rules are taken into account
-    run_firewall_test "--debug --allow-dns-request" "nc -zv -w 1 $ipv6_address 80"
-    assert_exit_code 1
-    assert_output_contains "WARN PACKET BLOCKED because=IPv6AlwaysBlocked blocked=true blockedAt=packet"
-close_fold
+if [ -z "${GITHUB_ACTIONS-}" ]; then
+    # Actions doesn't support ipv6 so can't run that test
+    open_fold "IPv6: Block IPv6 Traffic as not supported in the proxy currently (Allow DNS)"
+        ipv6_address=$(dig hostname A ifconfig.co AAAA +short | tail -n 1)
+        # Run it without blocking anything to ensure it's getting stopped before the ip rules are taken into account
+        run_firewall_test "--debug --allow-dns-request" "nc -zv -w 1 $ipv6_address 80"
+        assert_exit_code 1
+        assert_output_contains "WARN PACKET BLOCKED because=IPv6AlwaysBlocked blocked=true blockedAt=packet"
+    close_fold
+fi
 
 # Why on host only?
 # Docker in docker complicates attach tests
