6c83fd2c589b334ea867b46667c9e7f3c5fa9ca6
lawrencegripper <i+gitgen@grippers.co.uk>
2025-03-17 17:55:47 +0000

    Move out ipv6 to differnet script

--- script/smoke-test-host.sh
+++ script/smoke-test-host.sh
@@ -8,19 +8,6 @@ ps aux | grep './bin/ebpf-cgroup-firewall' | grep -v grep | awk '{print $2}' | x
 source "$(dirname "$0")/helpers.sh"
 
 # Why on host only?
-# Docker doesn't configure ipv6 by default, so we can't test it in the container
-if [ -z "${SKIP_IPV6-}" ]; then
-    # Actions doesn't support ipv6 so can't run that test
-    open_fold "IPv6: Block IPv6 Traffic as not supported in the proxy currently (Allow DNS)"
-        ipv6_address=$(dig hostname A ifconfig.co AAAA +short | tail -n 1)
-        # Run it without blocking anything to ensure it's getting stopped before the ip rules are taken into account
-        run_firewall_test "--debug --allow-dns-request" "nc -zv -w 1 $ipv6_address 80"
-        assert_exit_code 1
-        assert_output_contains "WARN PACKET BLOCKED because=IPv6AlwaysBlocked blocked=true blockedAt=packet"
-    close_fold
-fi
-
-# Why on host only?
 # Docker in docker complicates attach tests
 open_fold "Docker Attach block list: Curl google when blocked"
     attach_container_firewall_test "--block-list google.com" "curl $default_curl_args google.com"
